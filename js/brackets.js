1ef88f6f14eddf376a9bc1dd421582920382804d
Frank Santana <frigsamo@gmail.com>
2018-12-20 19:20:31 -0500

    Brackets

---
+++ js/brackets.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    var data = 
+    [
+        {
+            "tournamentNumber" : 1 ,
+            "matchNumber" : 1 ,
+            "player1Name" : "Flash" ,
+            "player2Name" : "Jaedong" ,
+            "player1Score" : null ,
+            "player2Score" : null ,
+            "winnerName" : null
+        } ,
+        {
+            "tournamentNumber" : 1 ,
+            "matchNumber" : 2 ,
+            "player1Name" : "Bisu" ,
+            "player2Name" : "Stork" ,
+            "player1Score" : null ,
+            "player2Score" : null ,
+            "winnerName" : null
+        } ,
+        {
+            "tournamentNumber" : 1 ,
+            "matchNumber" : 3 ,
+            "player1Name" : null ,
+            "player2Name" : null ,
+            "player1Score" : null ,
+            "player2Score" : null ,
+            "winnerName" : null
+        }
+    ]
+
+
+    var cards = $('.card-panel');
+    //cards.css('background-color', '#FF4000');
+    var player1= $('#P1');
+    player1.attr('value', 'Frank');
+ }
+)
